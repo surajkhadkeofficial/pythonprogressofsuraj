775dd887268513d509c20f5c6a2eb70a68119716
Suraj Khadke <164303790+surajkhadkeofficial@users.noreply.github.com>
2025-09-23 17:51:03 +0530

    Initial commit

---
+++ CHAPTER_3_STRINGS/problem04.py
@@ -0,0 +1,5 @@
+#replace double space with single space
+
+space = '''suraj is good boy  he learns new concepts with fun'''
+
+print(space.replace("  "," "))
---
+++ CHAPTER_3_STRINGS/problem05.py
@@ -0,0 +1,5 @@
+# Write a program to format the following letter using escape sequence characters.
+
+letter = "\"Dear Student,\tthis python course is nice. \nThanks!\""
+
+print(letter)
